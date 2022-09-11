006829451cf08eb3a4d1425a47b8dba94fbc3827
k-yutok <ciyt11058@g.nihon-u.ac.jp>
2022-09-11 16:15:22 +0900

    #対応中 コードを使用した画面遷移

---
+++ SampleApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SeondViewController.swift
+//  SampleApp
+//
+//  Created by yuutoku kouno on 2022/09/11.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController:UIViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
