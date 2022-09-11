2e150607a3d15120a9249be1fadf1bdc27910677
k-yutok <ciyt11058@g.nihon-u.ac.jp>
2022-09-11 15:05:46 +0900

    first

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by yuutoku kouno on 2022/09/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
