313e0d3002d1de176c9aec5d01f92170e04fdeb5
k-yutok <ciyt11058@g.nihon-u.ac.jp>
2022-09-11 16:46:47 +0900

    検索処理を追加

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -9,19 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BtnSearch: UIButton!
     @IBOutlet weak var BtnSecondView: UIButton!
     let vc = SecondViewController()
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         vc.modalPresentationStyle = .fullScreen
+        
     }
     
-    
+    func madeImage(){
+        
+    }
     @IBAction func goSecondView(_ sender: Any) {
         self.performSegue(withIdentifier: "Second", sender: nil)
      
+    }
+    
+    
+    @IBAction func searchLive(_ sender: Any) {
+        var dateLive = ""
+        
     }
     
 
